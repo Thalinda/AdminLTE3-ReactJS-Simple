d6ac7cf065613c572a1b2585cea04583eb1ca377
bthalinda <bthalinda@gmail.com>
2021-09-15 00:04:39 +0530

    Intialsetups

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Footer from './componets/common/footer'
+import Header from './componets/common/header'
+import Menu from './componets/common/menu'
+import Dashboard from './componets/pages/dashboard'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Menu></Menu>
+      <Dashboard/>
+      <Footer></Footer>
     </div>
-  );
+  )
 }
-
-export default App;
